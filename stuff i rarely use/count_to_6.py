0ea201a86523000350ad970993500a8a07f5e5af
ABHIJEET A S <abhijeet8120@gmail.com>
2021-12-23 23:49:27 +0530

    commit branch 1

--- stuff i rarely use/count_to_6.py
+++ stuff i rarely use/count_to_6.py
@@ -1,6 +1,8 @@
 for x in range(6):
     if (x == 3 or x==6):
         
-        print('nsdfhbjfs db')
+        print('nsdfhbjfs db giuadbfhjsklbdsbahfyudhxbhadsgyuiadsgvxyg')
+
+        print('bhdvfhsdbchxvhcsdk')
         continue
     print(x,end=' ')
